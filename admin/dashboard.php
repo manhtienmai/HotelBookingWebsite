8c52f1e4dd3d0607e1d713ff988840febdfaa9e3
Thangvu21 <vuducthang212004@gmail.com>
2023-10-07 16:29:56 +0700

    update Setting

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,11 +12,16 @@
   <?php require('inc/links.php'); ?>
 </head>
 <body class="bg-light">
-
-  <div class="container-fluid d-flex p-4 bg-dark text-light justify-content-between align-items-center ">
-    <h3 class="mb-0">ADMIN PANEL</h3>
-    <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+  <?php require('inc/header.php'); ?>
+  
+  <div class="container-fuild" id="main_content">
+    <div class="row">
+      <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aperiam neque atque dignissimos tempore distinctio debitis error, repellendus dolor consequuntur, corrupti accusamus eaque nulla sed consequatur, libero consectetur modi voluptatum optio corporis dicta tempora repudiandae sint assumenda? Unde impedit voluptatum deserunt nihil ratione quam voluptate ipsam obcaecati ipsa? Amet delectus voluptatum vero praesentium odio atque aliquid corrupti ut commodi, labore deleniti nemo cumque harum, assumenda exercitationem veritatis accusamus possimus sunt fugit voluptas provident adipisci, nobis eligendi deserunt. Doloremque culpa id quod quae, tenetur quam autem sint odit. Exercitationem rem animi voluptates reprehenderit quia nemo. Maxime pariatur eum voluptatum ad hic, odit quia quis quisquam quibusdam officiis, sapiente consectetur alias! Placeat quidem voluptatem sunt consequatur excepturi sequi praesentium sit quisquam, dolores blanditiis unde nam laudantium, molestiae voluptas modi non labore asperiores culpa, tempore facilis doloribus? Minima suscipit a rem ipsa in impedit alias provident totam vel unde, officia non. Dolor eos delectus sapiente autem reiciendis qui asperiores aperiam neque, voluptates architecto? Rem perspiciatis doloremque eaque et id optio perferendis facere deleniti laborum. Ipsum, soluta. Explicabo ducimus eum expedita iste officiis quidem voluptatibus temporibus, porro labore fugit alias itaque asperiores blanditiis commodi cupiditate. Soluta, accusamus ipsam. Quo doloremque placeat velit repellendus optio!
+      </div>
+    </div>
   </div>
+
   <?php require('inc/scripts.php') ?>
 </body>
 </html>
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,27 +1,70 @@
 <?php
+
+  // FRONTEND PURPOSE DATA
+  define('SITE_URL', 'http://127.0.0.1/HotelBookingWebsite/');
+  define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
+  
+
+  // BACKEND UPLOAD PROCESS NEEDS THIS DATA
+
+  define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'].'/HotelBookingWebsite/images/'); // ảnh trên web
+  define('ABOUT_FOLDER', 'about/'); // từ folder about
+
   function adminLogin() {
     session_start();
     if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)) {
       echo "<script>
         window.location.href='index.php';
       </script>";
+      exit;
     }
-    session_regenerate_id(true);
   }
   function redirect($url) {
     echo "<script>
             window.location.href='$url';  
           </script>";
-  
+          exit;
         }
+
+  // hàm bật thông báo
   function alert($type, $msg) {
     $bs_class = ($type == "success") ? "alert-success" : "alert-danger";
 
     echo <<<alert
       <div class="alert $bs_class alert-dismissible fade show custom-alert" role="alert">
-        <strong>$msg</strong>
+        <strong class="me-3">$msg</strong>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     alert;
+  }
+
+  function uploadImage($image, $folder) {
+    $valid_mine = ['image/jpeg', 'image/png', 'image/webp'];
+    $img_mine = $image['type'];
+
+    if (!in_array($img_mine, $valid_mine)) {
+      // kiểm tra type có cùng kiểu trong phần tử mảng trên không
+      return 'inv_img'; //invalid image mine or format
+    } else if (($image['size']/(1024*1024)) > 2) { // kiểm tra size
+      return 'inv_size'; // invalid size > 2mb
+    } else {
+      $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+      $rname = 'IMG_'.random_int(11111,99999).".$ext";
+      // vd: IMG_95555.png
+      $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+      if(move_uploaded_file($image['tmp_name'],$img_path)) {
+        return $rname;
+      } else {
+        return 'upd_failed';
+      }
+    }
+  }
+
+  function deleteImage($image, $folder) {
+    if (unlink(UPLOAD_IMAGE_PATH.$folder.$image)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 ?>
