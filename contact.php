0d29aa17b26ec73392544538c2fc04734bebb959
Quan Nguyen <quanlh3052004@gmail.com>
2023-10-08 21:56:18 +0700

    update by Quan Nguyen

--- contact.php
+++ contact.php
@@ -1,15 +1,17 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>JW MARIOTT - CONTACT</title>
-    <?php include('inc/links.php'); ?>
-  </head>
-  <body class="bg-light">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>JW MARIOTT - CONTACT</title>
+  <?php include('inc/links.php'); ?>
+</head>
+
+<body class="bg-light">
 
   <?php require('inc/header.php'); ?>
- 
+
   <div class="my-5 px-4">
     <h2 class="fw-bold h-font text-center">CONTACT US</h2>
     <div class="h-line"></div>
@@ -22,34 +24,48 @@
     <div class="row">
       <div class="col-lg-6 col-md-6 mb-5 px-4">
         <div class="bg-white rounded shadow p-4">
-          <iframe height="320px" class="w-100 rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.619997508281!2d105.78008017498027!3d21.00786438063624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acacbb621a31%3A0x6b9d241f84cd960!2sJW%20Marriott%20Hotel%20Hanoi!5e0!3m2!1sen!2s!4v1695991122665!5m2!1sen!2s" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe height="320px" class="w-100 rounded" src="<?php echo $contact_r['iframe'] ?>" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           <h5>Address</h5>
-          <a href="https://maps.app.goo.gl/zYTmm9jUQMLa27gdA" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
-            <i class="bi bi-geo-alt-fill"></i> Do Duc Duc, Tu Liem, HaNoi
+          <a href="<?php echo $contact_r['gmap']  ?>" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+            <i class="bi bi-geo-alt-fill"></i> <?php echo $contact_r['address'] ?>
           </a>
+
           <h5 class="mt-4">Call us</h5>
-            <a href="tel: +033699999" class="d-inline-block mb-2 text-decoration-none text-dark">
-              <i class="bi bi-telephone-fill"></i>+84999999999
-            </a>
-            <br>
-            <a href="tel: +033699999" class="d-inline-block mb-2 text-decoration-none text-dark">
-              <i class="bi bi-telephone-fill"></i>+848888888888
-            </a>
-            <h5 class="mt-4">Email</h5>
-            <a href="mailto: anonymous@gmail.com" class="d-inline-block text-decoration-none text-dark">
-              <i class="bi bi-envelope-fill"></i> Anonymous@gmai.com
-            </a>
-            <h5 class="mt-4">Follow us</h5>
-            <a href="" class="d-inline-block fs-5 me-2">
+          <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
+          </a>
+          <br>
+          <?php
+          if ($contact_r['pn2'] != '') {
+            echo <<<data
+                <a href="tel: +$contact_r[pn2]" class="d-inline-block mb-2 text-decoration-none text-dark">
+                  <i class="bi bi-telephone-fill"></i>+$contact_r[pn2]
+                </a>
+                data;
+          }
+          ?>
+          <h5 class="mt-4">Email</h5>
+          <a href="<?php echo $contact_r['email']?>" class="d-inline-block text-decoration-none text-dark">
+            <i class="bi bi-envelope-fill"></i> <?php echo $contact_r['email']?>
+          </a>
+
+          <h5 class="mt-4">Follow us</h5>
+          <?php
+            if($contact_r['tw'] != ''){
+              echo<<<data
+              <a href="$contact_r[tw]" class="d-inline-block text-dark fs-5 me-2">
                 <i class="bi bi-twitter me-1"></i>
-            </a>
-            <a href="" class="d-inline-block fs-5 me-2">
-                <i class="bi bi-facebook me-1"></i>
-            </a>
-            <a href="" class="d-inline-block">
-                <i class="bi bi-instagram me-1"></i>
-            </a>
-          </div>
+              </a>
+              data;
+            }
+          ?>
+          <a href="<?php echo $contact_r['fb']?>" class="d-inline-block text-dark fs-5 me-2">
+            <i class="bi bi-facebook me-1"></i>
+          </a>
+          <a href="<?php echo $contact_r['insta']?>" class="d-inline-block text-dark fs-5">
+            <i class="bi bi-instagram me-1"></i>
+          </a>
+        </div>
       </div>
       <div class="col-lg-6 col-md-6 mb-5 px-4">
         <div class="bg-white rounded shadow p-4">
@@ -80,5 +96,6 @@
 
   <?php include('inc/footer.php'); ?>
 
-  </body>
+</body>
+
 </html>
