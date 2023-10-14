930837fd2bd089904b3e67af3570c420c217be15
Quan Nguyen <quanlh3052004@gmail.com>
2023-10-14 15:44:30 +0700

    update user queries

--- contact.php
+++ contact.php
@@ -69,30 +69,47 @@
       </div>
       <div class="col-lg-6 col-md-6 mb-5 px-4">
         <div class="bg-white rounded shadow p-4">
-          <form>
+          <form method="POST">
             <h5>Send a message</h5>
             <div class="mt-3">
               <label class="form-label" style="font-weight: 500;">Name</label>
-              <input type="text" class="form-control shadow-none">
+              <input name="name" required type="text" class="form-control shadow-none">
             </div>
             <div class="mt-3">
               <label class="form-label" style="font-weight: 500;">Email</label>
-              <input type="email" class="form-control shadow-none">
+              <input name="email" required type="email" class="form-control shadow-none">
             </div>
             <div class="mt-3">
               <label class="form-label" style="font-weight: 500;">Subject</label>
-              <input type="text" class="form-control shadow-none">
+              <input name="subject" required type="text" class="form-control shadow-none">
             </div>
             <div class="mt-3">
               <label class="form-label" style="font-weight: 500;">Message</label>
-              <textarea rows="5" class="form-control shadow-none" style="resize: none;"></textarea></textarea>
-              <button type="submit" class="btn text-white custom-bg mt-3">Send</button></button>
+              <textarea name="message" required class="form-control shadow-none" rows="5" style="resize: none;"></textarea></textarea>
             </div>
+            <button type="submit" name="send" class="btn text-white custom-bg mt-3">Send</button></button>
           </form>
         </div>
       </div>
     </div>
   </div>
+
+  <?php
+    if(isset($_POST['send']))
+    {
+      $frm_data = filteration($_POST);
+
+      $q = "INSERT INTO `user_queries`( `name`, `email`, `subject`, `message`) VALUES (?,?,?,?)";
+      $values = [$frm_data['name'], $frm_data['email'],$frm_data['subject'],$frm_data['message']];
+      
+      $res = insert($q,$values,'ssss');
+      if($res==1){
+        alert('success', 'Mail sent!');
+      }else{
+        alert('error', ' Server Down ! Try again later.');
+      }
+    } 
+  ?>
 
   <?php include('inc/footer.php'); ?>
 
