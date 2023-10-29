236dcfd9c346e352c2a02084bce48df441e9c9be
Thangvu21 <vuducthang212004@gmail.com>
2023-10-29 16:07:44 +0700

    update rooms

--- admin/ajax/features_facilities.php
+++ admin/ajax/features_facilities.php
@@ -40,9 +40,18 @@ if (isset($_POST['rem_feature'])) {
     $frm_data = filteration($_POST);
     $values = [$frm_data['rem_feature']];
 
-    $q = "DELETE FROM `features` WHERE `id` = ?";
-    $res = delete($q, $values, 'i');
-    echo $res;
+    // kieeu kiem tra ket noi voi bang khac chua
+    $check_q = select('SELECT * FROM `room_features` WHERE `features_id` = ?', [$frm_data['rem_features']], 'i');
+
+    if (mysqli_num_rows($check_q) == 0) {
+        $q = "DELETE FROM `features` WHERE `id` = ?";
+        $res = delete($q, $values, 'i');
+        echo $res;
+    } else {
+        echo 'room_added';
+    }
+
+    
 }
 //----------------------------------
 
@@ -93,20 +102,22 @@ if (isset($_POST['rem_facility'])) {
     $frm_data = filteration($_POST);
     $values = [$frm_data['rem_facility']];
 
-    $pre_q = "SELECT * FROM `facilities` WHERE `id` = ?";
-    $res = select($pre_q, $values, 'i');
-    $img = mysqli_fetch_assoc($res);
+    $check_q = select('SELECT * FROM `room_facilities` WHERE `facilities_id` = ?', [$frm_data['rem_facility']], 'i');
+    if (mysqli_num_rows($check_q) == 0) {
+        $pre_q = "SELECT * FROM `facilities` WHERE `id` = ?";
+        $res = select($pre_q, $values, 'i');
+        $img = mysqli_fetch_assoc($res);
 
-    if(deleteImage($img['icon'], FACILITIES_FOLDER)) {
-        $q = "DELETE FROM `facilities` WHERE `id` = ?";
-        $res = delete($q, $values, 'i');
-        echo $res;
+        if(deleteImage($img['icon'], FACILITIES_FOLDER)) {
+            $q = "DELETE FROM `facilities` WHERE `id` = ?";
+            $res = delete($q, $values, 'i');
+            echo $res;
+        } else {
+            echo 0;
+        }
     } else {
-        echo 0;
-    }
-
-
-   
+        echo 'room_added';
+    }   
 }
 
 
